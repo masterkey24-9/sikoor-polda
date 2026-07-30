06fbeb8c613558af904ea0634a410ef2ccb214fc
YUKE23333 <apisdisini8@gmail.com>
2026-07-30 14:12:37 +0700

    ...

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\IndicatorController;
 use App\Http\Controllers\IndicatorResultController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SatkerController;
 
 Route::get('/', function () {
     return view('auth.login');
@@ -35,6 +36,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/satkers', [SatkerController::class, 'index'])->name('satkers.index');
+    Route::post('/satkers', [SatkerController::class, 'store'])->name('satkers.store');
+    Route::delete('/satkers/{id}', [SatkerController::class, 'destroy'])->name('satkers.destroy');
 });
 
 require __DIR__.'/auth.php';
